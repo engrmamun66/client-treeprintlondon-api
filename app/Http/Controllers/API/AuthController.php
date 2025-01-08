98c7d76dadb60d50211639faa5ed444f6fecc2b6
Asaduzzaman Rabby <rabby@leapinglogic.com>
2025-01-08 11:16:39 +0600

    send auth user information

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -103,7 +103,8 @@ class AuthController extends BaseController
         return [
             'access_token' => $token,
             'token_type' => 'bearer',
-            'expires_in' => auth()->factory()->getTTL() * 60
+            'expires_in' => auth()->factory()->getTTL() * 60,
+            'user' => auth()->user()
         ];
     }
 }
