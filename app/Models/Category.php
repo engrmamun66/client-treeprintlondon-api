0b4feda08360c5602353d0da19bb1605a6bd81c0
Asaduzzaman Rabby <rabby@leapinglogic.com>
2025-01-09 16:59:28 +0600

    show image full url

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,13 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Category extends Model
 {
     protected $fillable = ['name', 'slug', 'description', 'image', 'parent_id', 'status'];
 
     protected $appends = ['image_url'];
-    
+
     // Define the parent category relationship
     public function parent()
     {
