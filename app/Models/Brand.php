36e73f1275bca682ac5c179a5b12425dbb134e07
Asaduzzaman Rabby <rabby@leapinglogic.com>
2025-01-11 14:15:41 +0600

    brand model image url get using accessors

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Brand extends Model
 {
     protected $guarded = [];
+    protected $appends = ['image_url'];
+    public function getImageUrlAttribute()
+    {
+        return $this->image 
+            ? asset('storage/' . $this->image)
+            : null;
+    }
 }
