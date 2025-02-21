efbdd4622e5e623bd5fc0cac941a7ee5dec116b4
Asaduzzaman Rabby <rabby@leapinglogic.com>
2025-02-21 23:46:40 +0600

    pivot table data added

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -34,7 +34,7 @@ class Product extends Model
     }
     public function sizes()
     {
-        return $this->belongsToMany(Size::class, 'product_sizes');
+        return $this->belongsToMany(Size::class, 'product_sizes')->withPivot(['unit_price', 'quantity', 'discounted_unit_price']);
     }
     public function genders()
     {
